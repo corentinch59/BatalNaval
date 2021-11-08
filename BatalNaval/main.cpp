47a0bd67eb83002e408a2ce3afdcdf4fc8874f11
Macéo Prévost <maceo.prevost@outlook.fr>
2021-11-08 17:41:05 +0100

    add gameloop

--- BatalNaval/main.cpp
+++ BatalNaval/main.cpp
@@ -4,6 +4,45 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "ChronoSpacer");
+    // Initialise everything below
+
+    sf::CircleShape circle; // Déclaration de "circle" (sans affectation)
+    circle.setRadius(100.0f);
+    circle.setPosition(100.0f, 100.0f);
+    circle.setFillColor(sf::Color::Red); // Appel de méthode qui travaille sur "circle"
+
+
+    // Game loop
+    while (window.isOpen()) {
+        sf::Event event;
+
+        while (window.pollEvent(event)) {
+            // Process any input event here
+
+            switch (event.type) {
+
+            case sf::Event::Closed:
+                window.close();
+                break;
+
+            case sf::Event::KeyPressed:
+                if (event.key.code == sf::Keyboard::Space)
+                {
+                }
+
+            default: break;
+            }
+        }
+        window.clear();
+        // Whatever I want to draw goes here
+        window.draw(circle);
+
+        window.display();
+    }
+
+
+
 }
 
