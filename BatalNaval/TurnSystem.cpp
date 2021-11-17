284e1d060a89068c90c230917ab5204794641608
Cocomineral <tintinch59@gmail.com>
2021-11-17 16:16:38 +0100

    - Started creating TurnSystem

---
+++ BatalNaval/TurnSystem.cpp
@@ -0,0 +1,33 @@
+
+#include "TurnSystem.h"
+#include "batal.h"
+
+std::vector<Batal> InitialiseList(Batal& player1, Batal& player2) {
+	std::vector<Batal> newList;
+
+	newList.push_back(player1);
+	newList.push_back(player2);
+
+	return newList;
+}
+
+std::vector<Batal>::iterator InitialiseTurn(std::vector<Batal> list) {
+	std::vector<Batal>::iterator it;
+
+	return it;
+}
+
+void StartingPhase(std::vector<Batal>::iterator& it) {
+	//TODO: start the phase check if fishing or batal on fire.
+}
+
+void ActionPhase(std::vector<Batal>::iterator& it) {
+	/*TODO: wait for player input between :
+	- Shoot
+	- Rain
+	- Start fishing*/
+}
+
+std::vector<Batal>::iterator EndPhase(std::vector<Batal>::iterator& it) {
+	//TODO: change iterator to the other batal
+}
